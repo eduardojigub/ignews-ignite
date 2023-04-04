5df583222abb9b9689963b07448532b98d0e6eec
eduardojigub <eduardobrunoit@gmail.com>
2023-04-03 21:47:43 -0300

    feat: titulo dinâmico na páginaC

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import styles from '../styles/home.module.scss';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
       <h1 className={styles.title}>Hello World</h1>
     </>
   );
