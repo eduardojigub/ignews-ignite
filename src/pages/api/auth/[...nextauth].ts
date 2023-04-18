53566ee3e9e0cc83f8395db41be1de6b756e6f26
eduardojigub <eduardobrunoit@gmail.com>
2023-04-17 21:19:44 -0300

    fix: process.env calls

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,12 @@
 import NextAuth from 'next-auth'
 import GitHubProvider from "next-auth/providers/github";
 
-import {query as q} from 'faunadb'
-import { fauna } from '../../../services/fauna'
-import { signIn } from 'next-auth/react';
-
 export default NextAuth({
     // Configure one or more authentication providers
     providers: [
       GitHubProvider({
-        clientId: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        clientId: process.env.GITHUB_CLIENT_ID as string,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
         authorization: {
           params: {
             scope: 'read:user',
@@ -18,38 +14,4 @@ export default NextAuth({
         },
       }),
     ],
-      callbacks: {
-        async signIn({ user }) {
-          const { email } = user
-
-          try {
-            await fauna.query(
-              q.If(
-                q.Not(
-                  q.Exists(
-                    q.Match(
-                      q.Index('user_by_email'),
-                      q.Casefold(user.email)
-                    )
-                  )
-                ),
-                q.Create(
-                  q.Collection('users'),
-                  { data: { email } }
-              ),
-              q.Get(
-                q.Match(
-                  q.Index('user_by_email'),
-                  q.Casefold(user.email)
-                  )
-                )
-              )
-            )
-            return true
-          }
-          catch {
-            return false
-          }
-        },
-      }
-})
+  });
