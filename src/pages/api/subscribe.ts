fa6dc94a1190edd0bddba8f1864df832d150669c
eduardojigub <eduardobrunoit@gmail.com>
2023-04-17 22:28:03 -0300

    feat: avoid duplicate costumers on stripe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,6 +1,17 @@
 import { stripe } from "@/services/stripe";
+import { query as q} from 'faunadb'
 import { getSession } from "next-auth/react";
 import { NextApiRequest, NextApiResponse } from "next";
+import { fauna } from "@/services/fauna";
+
+type User = {
+    ref: {
+        id: string;
+    }
+    data: {
+        stripe_customer_id: string;
+    }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
@@ -11,6 +22,48 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             email: session.user.email,
             // metadata
         })
+
+        const user = await fauna.query<User>(
+            q.Get(
+                q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(session.user.email)
+                )
+            )
+        )
+
+        await fauna.query(
+            q.Update(
+                q.Ref(q.Collection('users'), user.ref.id),
+                {
+                    data: {
+                        stripe_customer_id: stripeCustomer.id
+                    }
+                }
+            )
+        )
+
+        let customerId = user.data.stripe_customer_id
+
+        if(!customerId) {
+            const stripeCustomer = await stripe.customers.create({
+                email: session.user.email,
+                // metadata
+            })
+
+            await fauna.query(
+                q.Update(
+                    q.Ref(q.Collection('users'), user.ref.id),
+                    {
+                        data: {
+                            stripe_customer_id: stripeCustomer.id
+                        }
+                    }
+                )
+            )
+
+            customerId = stripeCustomer.id
+        }
 
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
             customer: stripeCustomer.id,
